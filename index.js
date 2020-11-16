6f358f70ec481a750b387fe1d771330b82b71d0f
hugodelgado <hugo.delgado1@outlook.com>
2020-11-16 15:38:43 -0600

    commit tree d7aadebd7074d6043b915df87960ae7c152a3553

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution in this file!
+let block = 0
+function distanceFromHqInBlocks (distance) {
+    if (distance == 43) {
+    block = 1;
+    return block
+    } else if (distance == 50) {
+    block = 8;
+    return block
+    } else if (distance == 34) {
+    block = 8
+    return block
+    }
+}
